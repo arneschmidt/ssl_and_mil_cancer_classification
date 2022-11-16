771d4bd3915ad477e7dbae7e207b31652df92f3e
arne <arne@decsai.ugr.es>
2022-11-16 12:31:44 +0100

    add prostate_ugr configurations

---
+++ dataset_dependent/prostate_ugr/experiments/run.sh
@@ -0,0 +1,3 @@
+python src/main.py  -dc ../../experiments/prostate_ugr/experiments/config.yaml -ec ../../experiments/prostate_ugr/experiments/retraining/config_train.yaml
+python src/main.py  -dc ../../experiments/prostate_ugr/experiments/config.yaml -ec ../../experiments/prostate_ugr/experiments/retraining/config_test.yaml
+python src/main.py  -dc ../../experiments/prostate_ugr/experiments/config.yaml -ec ../../experiments/prostate_ugr/experiments/retraining/config_predict.yaml
