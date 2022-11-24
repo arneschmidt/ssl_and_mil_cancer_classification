541cfc0516768d7d7a191c7953e8e835f725be81
arne <arne@decsai.ugr.es>
2022-11-24 08:52:51 +0100

    fix paths

--- dataset_dependent/prostate_ugr/experiments/run.sh
+++ dataset_dependent/prostate_ugr/experiments/run.sh
@@ -1,3 +1,3 @@
-python src/main.py  -dc ./dataset_dependent/prostate_ugr/experiments/config.yaml -ec ../../experiments/prostate_ugr/experiments/retraining/config_train.yaml
-python src/main.py  -dc ./dataset_dependent/prostate_ugr/experiments/config.yaml -ec ../../experiments/prostate_ugr/experiments/retraining/config_test.yaml
-python src/main.py  -dc ./dataset_dependent/prostate_ugr/experiments/config.yaml -ec ../../experiments/prostate_ugr/experiments/retraining/config_predict.yaml
+python src/main.py  -dc ./dataset_dependent/prostate_ugr/experiments/config.yaml -ec ./dataset_dependent/prostate_ugr/experiments/retraining/config_train.yaml
+python src/main.py  -dc ./dataset_dependent/prostate_ugr/experiments/config.yaml -ec ./dataset_dependent/prostate_ugr/experiments/retraining/config_test.yaml
+python src/main.py  -dc ./dataset_dependent/prostate_ugr/experiments/config.yaml -ec ./dataset_dependent/prostate_ugr/experiments/retraining/config_predict.yaml
